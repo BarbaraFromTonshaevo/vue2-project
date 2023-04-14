898ca132c42939ae1b2607bbc2cf15097d856962
Elkina Varvara <elkinavarvara1999@gmail.com>
2023-04-14 14:08:41 +0300

    cart page

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import MainPage from "@/pages/MainPage";
 import ProductPage from "@/pages/ProductPage";
 import NotFoundPage from "@/pages/NotFoundPage";
+import CartPage from "@/pages/CartPage";
 import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 const routes = [
   { name: "main", component: MainPage, path: "/" },
   { name: "product", component: ProductPage, path: "/product/:id" },
+  { name: "cart", component: CartPage, path: "/cart" },
   { name: "notFound", component: NotFoundPage, path: "*" },
 ];
 const router = new VueRouter({ routes });

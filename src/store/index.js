758ee877555fba3ac88ac87c975d6d4b4e2f1dc0
Elkina Varvara <elkinavarvara1999@gmail.com>
2023-04-21 14:07:02 +0300

    cart page

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import { products } from "@/data/products";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    cartProducts: [{ productId: 1, amount: 2 }],
+    cartProducts: [],
   },
   mutations: {
     addProductToCart(state, { productId, amount }) {
@@ -20,6 +20,35 @@ export default new Vuex.Store({
           amount,
         });
       }
+    },
+    updateCartProductAmount(state, { productId, amount }) {
+      const item = state.cartProducts.find(
+        (item) => item.productId === productId
+      );
+      if (item) {
+        item.amount = amount;
+      }
+    },
+    deleteCartProduct(state, productId) {
+      state.cartProducts = state.cartProducts.filter(
+        (item) => item.productId !== productId
+      );
+    },
+  },
+  getters: {
+    cartDetailProducts(state) {
+      return state.cartProducts.map((item) => {
+        return {
+          ...item,
+          product: products.find((p) => p.id === item.productId),
+        };
+      });
+    },
+    cartTotalPrice(state, getters) {
+      return getters.cartDetailProducts.reduce(
+        (acc, item) => item.product.price * item.amount + acc,
+        0
+      );
     },
   },
 });
